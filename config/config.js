1078db5cdf80c265ac542fa34eefe04e54699daf
faisal-zaheervi <faisalszaheer@gmail.com>
2021-11-10 14:03:20 -0800

    changes

--- config/config.js
+++ config/config.js
@@ -11,9 +11,6 @@ module.exports = {
   },
   production: {
     use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: true
-    }
+    dialect: "postgres"
   }
 };
