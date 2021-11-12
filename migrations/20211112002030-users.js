7f13c230d8a20126351e1ed269f3a78e872aa3d6
Faisal-ZaheerVI <FaisalSZaheer@gmail.com>
2021-11-11 17:19:24 -0800

    Added users migration

---
+++ migrations/20211112002030-users.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      'users',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        username: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        password: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("NOW()"),
+          allowNull: false
+        }
+      }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
