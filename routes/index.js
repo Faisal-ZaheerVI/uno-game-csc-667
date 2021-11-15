1bcfb021b05d5afdc94059488d9dab14bcad1af0
faisal-zaheervi <faisalszaheer@gmail.com>
2021-11-14 19:24:02 -0800

    progress

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,23 @@ router.get('/', function(req, res, next) {
   res.render('home');
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
-
-router.post('/login', function(req, res, next) {
-  res.redirect('/lobby');
-});
-
 router.get('/registration', function(req, res, next) {
   res.render('registration');
 });
 
-router.post('/registration', function(req, res, next) {
-  res.redirect('lobby');
+// router.post('/users/register', function(req, res, next) {
+//   let {username, email, password, cpassword} = req.body;
+
+//   console.log({username, email, password, cpassword});
+// });
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
 });
+
+// router.post('/login', function(req, res, next) {
+//   res.redirect('/lobby');
+// });
 
 router.get('/rules', function(req, res, next) {
   res.render('rules');
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const UserModel = require('../models/Users');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/* REGISTER */
+router.post('/users/register', (req, res, next) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  let cpassword = req.body.cpassword;
+
+  console.log({username, email, password, cpassword});
 });
 
 module.exports = router;
