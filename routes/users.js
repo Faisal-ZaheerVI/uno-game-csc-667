810df818c0cd69953fe7aaba1b8d2847c163e58a
faisal-zaheervi <faisalszaheer@gmail.com>
2021-11-14 22:00:13 -0800

    Fixed users route

--- routes/users.js
+++ routes/users.js
@@ -8,43 +8,43 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// /* REGISTER */
-// router.post('/register', (req, res, next) => {
-//   let username = req.body.username;
-//   let email = req.body.email;
-//   let password = req.body.password;
-//   let cpassword = req.body.cpassword;
+/* REGISTER */
+router.post('/register', (req, res, next) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  let cpassword = req.body.cpassword;
 
-//   // Simple Form Validation:
-//   // Username: At least 3 characters
-//   // Email: 
-//   // Password: At least 6 characters
-//   // Password + Confirm Password needs to match
+  // Simple Form Validation:
+  // Username: At least 3 characters
+  // Email: 
+  // Password: At least 6 characters
+  // Password + Confirm Password needs to match
 
-//   let errors = [];
+  let errors = [];
 
-//   if (!username || !email || !password || !cpassword) {
-//     errors.push({message: "Please enter all fields."});
-//   }
+  if (!username || !email || !password || !cpassword) {
+    errors.push({message: "Please enter all fields."});
+  }
 
-//   if (username.length < 3) {
-//     errors.push({message: "Username must be at least 3 characters long."});
-//   }
+  if (username.length < 3) {
+    errors.push({message: "Username must be at least 3 characters long."});
+  }
 
-//   if (password.length < 6) {
-//     errors.push({message: "Password must be at least 6 characters long."});
-//   }
+  if (password.length < 6) {
+    errors.push({message: "Password must be at least 6 characters long."});
+  }
 
-//   if (password != cpassword) {
-//     errors.push({message: "Passwords do not match."});
-//   }
+  if (password != cpassword) {
+    errors.push({message: "Passwords do not match."});
+  }
 
-//   if (errors.length > 0) {
-//     res.render('registration', { errors });
-//   } else {
+  if (errors.length > 0) {
+    res.render('registration', { errors });
+  } else {
 
-//   }
+  }
 
-// });
+});
 
 module.exports = router;
