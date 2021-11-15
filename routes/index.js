07b14d054da6a582170ae709597f517467ff05e3
faisal-zaheervi <faisalszaheer@gmail.com>
2021-11-14 21:56:48 -0800

    Added simple registration validation

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,6 @@ router.get('/registration', function(req, res, next) {
   res.render('registration');
 });
 
-// router.post('/users/register', function(req, res, next) {
-//   let {username, email, password, cpassword} = req.body;
-
-//   console.log({username, email, password, cpassword});
-// });
-
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
@@ -33,6 +27,45 @@ router.get('/lobby', function(req, res, next) {
 
 router.get('/game', function(req, res, next) {
   res.render('game');
+});
+
+/* REGISTER */
+router.post('/users/register', (req, res, next) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  let cpassword = req.body.cpassword;
+
+  // Simple Form Validation:
+  // Username: At least 3 characters
+  // Email: 
+  // Password: At least 6 characters
+  // Password + Confirm Password needs to match
+
+  let errors = [];
+
+  if (!username || !email || !password || !cpassword) {
+    errors.push({message: "Please enter all fields."});
+  }
+
+  if (username.length < 3) {
+    errors.push({message: "Username must be at least 3 characters long."});
+  }
+
+  if (password.length < 6) {
+    errors.push({message: "Password must be at least 6 characters long."});
+  }
+
+  if (password != cpassword) {
+    errors.push({message: "Passwords do not match."});
+  }
+
+  if (errors.length > 0) {
+    res.render('registration', { errors });
+  } else {
+
+  }
+
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -8,14 +8,43 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/* REGISTER */
-router.post('/users/register', (req, res, next) => {
-  let username = req.body.username;
-  let email = req.body.email;
-  let password = req.body.password;
-  let cpassword = req.body.cpassword;
+// /* REGISTER */
+// router.post('/register', (req, res, next) => {
+//   let username = req.body.username;
+//   let email = req.body.email;
+//   let password = req.body.password;
+//   let cpassword = req.body.cpassword;
 
-  console.log({username, email, password, cpassword});
-});
+//   // Simple Form Validation:
+//   // Username: At least 3 characters
+//   // Email: 
+//   // Password: At least 6 characters
+//   // Password + Confirm Password needs to match
+
+//   let errors = [];
+
+//   if (!username || !email || !password || !cpassword) {
+//     errors.push({message: "Please enter all fields."});
+//   }
+
+//   if (username.length < 3) {
+//     errors.push({message: "Username must be at least 3 characters long."});
+//   }
+
+//   if (password.length < 6) {
+//     errors.push({message: "Password must be at least 6 characters long."});
+//   }
+
+//   if (password != cpassword) {
+//     errors.push({message: "Passwords do not match."});
+//   }
+
+//   if (errors.length > 0) {
+//     res.render('registration', { errors });
+//   } else {
+
+//   }
+
+// });
 
 module.exports = router;
