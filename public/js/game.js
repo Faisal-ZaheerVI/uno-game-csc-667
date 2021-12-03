c6b7fcd4ae32a9882d4a8d063839517e605200a1
michaelsatumba <mjsatumba@gmail.com>
2021-12-03 10:56:32 -0800

    button styles and clicking card functionality

---
+++ public/js/game.js
@@ -0,0 +1,78 @@
+// function showAlert() {
+//     var myText = "This can be whatever text you like!";
+//     alert (myText);
+//   }
+
+
+
+
+
+// function random() {
+
+
+//     var x = Math.floor(Math.random() * 9) + 0;
+
+//     $(".middle-card-image").attr("src", "assets" + "/Blue_" + x + ".png");
+    
+// }
+
+function cardToCenter() {
+
+  var source = $(".firstCard").attr("src");
+ 
+
+  $(".middle-card-image").attr("src", source);
+    
+}
+
+function cardToCenter2() {
+
+  var source = $(".secondCard").attr("src");
+ 
+
+  $(".middle-card-image").attr("src", source);
+    
+}
+
+function cardToCenter3() {
+
+  var source = $(".thirdCard").attr("src");
+ 
+
+  $(".middle-card-image").attr("src", source);
+    
+}
+function cardToCenter4() {
+
+  var source = $(".fourthCard").attr("src");
+ 
+
+  $(".middle-card-image").attr("src", source);
+    
+}
+function cardToCenter5() {
+
+  var source = $(".fifthCard").attr("src");
+ 
+
+  $(".middle-card-image").attr("src", source);
+    
+}
+function cardToCenter6() {
+
+  var source = $(".sixthCard").attr("src");
+ 
+
+  $(".middle-card-image").attr("src", source);
+    
+}
+function cardToCenter7() {
+
+  var source = $(".seventhCard").attr("src");
+ 
+
+  $(".middle-card-image").attr("src", source);
+    
+}
+
+
