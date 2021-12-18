f3ff4ced94f1d5786689d6c768c4c41ec778f37a
faisal-zaheervi <faisalszaheer@gmail.com>
2021-12-17 22:54:04 -0800

    Heroku attempt

--- config/config.js
+++ config/config.js
@@ -12,7 +12,6 @@ module.exports = {
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
-    ssl: { rejectUnauthorized: false },
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false,
